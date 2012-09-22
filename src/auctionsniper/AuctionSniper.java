51e05f66e8df087128b567c7870d2ad430b170be
Nien-Kun Yeh <ynk0917@gmail.com>
2012-09-23 01:17:24 +0800

    pass AuctionSniperTest

--- src/auctionsniper/AuctionSniper.java
+++ src/auctionsniper/AuctionSniper.java
@@ -1,11 +1,24 @@
 package auctionsniper;
 
-public class AuctionSniper {
+public class AuctionSniper implements AuctionEventListener {
+    private final SniperListener sniperListener;
 
     public AuctionSniper(SniperListener sniperListener) {
+        this.sniperListener = sniperListener;
     }
 
     public void auctionCloesd() {
+        sniperListener.sniperLost();
+    }
+
+    @Override
+    public void auctionClosed() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void currentPrice(int price, int incerment) {
         // TODO Auto-generated method stub
         
     }
