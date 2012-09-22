df308eaef77452e296feeaa13a1913218ab36309
Nien-Kun Yeh <ynk0917@gmail.com>
2012-09-23 02:32:00 +0800

    move XMPPAuction out

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -6,10 +6,8 @@ import java.awt.event.WindowEvent;
 import javax.swing.SwingUtilities;
 
 import org.jivesoftware.smack.Chat;
-import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.packet.Message;
 
 public class Main implements SniperListener {
 
@@ -99,31 +97,5 @@ public class Main implements SniperListener {
             }
         });
         
-    }
-    
-    public static class XMPPAuction implements Auction {
-        private final Chat chat;
-        
-        public XMPPAuction(Chat chat) {
-            this.chat = chat;
-        }
-
-        @Override
-        public void bid(int amount) {
-            sendMessage(String.format(BID_COMMAND_FORMAT, amount));
-        }
-
-        @Override
-        public void join() {
-            sendMessage(JOIN_COMMAND_FORMAT);
-        }
-        
-        private void sendMessage(final String message) {
-            try {
-                chat.sendMessage(message);
-            } catch (XMPPException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
