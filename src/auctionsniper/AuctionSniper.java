91a0b3e9c672b0909fb4211481349faab51e43d0
Nien-Kun Yeh <ynk0917@gmail.com>
2012-09-23 01:13:51 +0800

    add AuctionSniperTest

---
+++ src/auctionsniper/AuctionSniper.java
@@ -0,0 +1,13 @@
+package auctionsniper;
+
+public class AuctionSniper {
+
+    public AuctionSniper(SniperListener sniperListener) {
+    }
+
+    public void auctionCloesd() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
