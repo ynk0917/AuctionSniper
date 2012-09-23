5cd48bc10c78419352b73b602885a6d867b39244
Nien-Kun Yeh <ynk0917@gmail.com>
2012-09-23 16:16:33 +0800

    add unit test
    add reportsLostIfAuctionClosesWhenBidding

--- test/unit/test/auctionsniper/AuctionSniperTest.java
+++ test/unit/test/auctionsniper/AuctionSniperTest.java
@@ -1,15 +1,15 @@
 package test.auctionsniper;
 
-import javax.swing.JTable.PrintMode;
-
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.States;
 import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import auctionsniper.Auction;
 import auctionsniper.AuctionEventListener;
+import auctionsniper.AuctionEventListener.PriceSource;
 import auctionsniper.AuctionSniper;
 import auctionsniper.SniperListener;
 
@@ -19,12 +19,26 @@ public class AuctionSniperTest {
     private final SniperListener sniperListener = context.mock(SniperListener.class);
     private final Auction auction = context.mock(Auction.class);
     private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
+    private final States sniperState = context.states("sniper");
 
     @Test
-    public void reportsLostWhenAuctionCloses() {
+    public void reportsLostWhenAuctionClosesImmediately() {
         context.checking(new Expectations() {{
             one(sniperListener).sniperLost();
         }});
+        sniper.auctionClosed();
+    }
+    
+    @Test
+    public void reportsLostIfAuctionClosesWhenBidding() {
+        context.checking(new Expectations() {{
+            ignoring(auction);
+            allowing(sniperListener).sniperBidding();
+            then(sniperState.is("bidding"));
+            atLeast(1).of(sniperListener).sniperLost();
+            when(sniperState.is("bidding"));
+        }});
+        sniper.currentPrice(123, 45, PriceSource.FromOtherBidder);
         sniper.auctionClosed();
     }
 
