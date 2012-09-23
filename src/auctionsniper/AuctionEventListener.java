87c8c2a2132b7326f338fd99bb27c1b44036c9fc
Nien-Kun Yeh <ynk0917@gmail.com>
2012-09-23 15:33:54 +0800

    add Price Source

--- src/auctionsniper/AuctionEventListener.java
+++ src/auctionsniper/AuctionEventListener.java
@@ -2,6 +2,10 @@ package auctionsniper;
 
 public interface AuctionEventListener {
 
+    enum PriceSource {
+        FromSniper, FromOtherBidder;
+    };
+    
     public void auctionClosed();
     public void currentPrice(int price, int incerment);
 }
