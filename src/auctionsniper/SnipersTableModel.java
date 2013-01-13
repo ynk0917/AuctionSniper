e10e56e49d87e7d123b2cb0fe5f5df37efe8160d
Nien-Kun Yeh <ynk0917@gmail.com>
2013-01-13 15:28:35 +0800

    OO for showing Column's value.

--- src/auctionsniper/SnipersTableModel.java
+++ src/auctionsniper/SnipersTableModel.java
@@ -19,21 +19,10 @@ public class SnipersTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        switch (Column.at(columnIndex)) {
-        case ITEM_IDENTIFIER:
-            return snapshot.itemId;
-        case LAST_PRICE:
-            return snapshot.lastPrice;
-        case LAST_BID:
-            return snapshot.lastBid;
-        case SNIPER_STATE:
-            return textFor(snapshot.state);
-        default:
-            throw new IllegalArgumentException("No column at " + columnIndex);
-        }
+        return Column.at(columnIndex).valueIn(snapshot);
     }
     
-    private static String textFor(SniperState state) {
+    static String textFor(SniperState state) {
         return STATUS_TEXT[state.ordinal()];
     }
     
@@ -41,5 +30,4 @@ public class SnipersTableModel extends AbstractTableModel {
         snapshot = newSnapshot;
         fireTableRowsUpdated(0, 0);
     }
-
 }
