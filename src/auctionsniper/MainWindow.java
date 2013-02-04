be36d13278255f80c5cbcedea4e4f21ef373d586
Nien-Kun Yeh <ynk0917@gmail.com>
2013-02-04 17:14:04 +0800

    pass integration test

--- src/auctionsniper/MainWindow.java
+++ src/auctionsniper/MainWindow.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -27,6 +29,8 @@ public class MainWindow extends JFrame {
     public static final String STATUS_WON = "Won";
     public static final String NEW_ITEM_ID_NAME = "item id";
     public static final String JOIN_BUTTON_NAME = "join button";
+    
+    private final Announcer<UserRequestListener> userRequests = Announcer.to(UserRequestListener.class);
     
     public MainWindow(SnipersTableModel snipers) {
         super(APPLICATION_TITLE);
@@ -59,11 +63,19 @@ public class MainWindow extends JFrame {
         
         JButton joinAuctionButton = new JButton("Join Auction");
         joinAuctionButton.setName(JOIN_BUTTON_NAME);
+        joinAuctionButton.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userRequests.announce().joinAuction(itemIdField.getText());
+            }
+        });
         controls.add(joinAuctionButton);
         
         return controls;
     }
 
     public void addUserRequestListener(UserRequestListener userRequestListener) {
+        userRequests.addListener(userRequestListener);
     }
 }
